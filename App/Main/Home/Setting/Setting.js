1c36839eaedc0e60f6da2d7668c5eee656f5e30f
borra <ryle2724@gmail.com>
2022-06-13 20:22:42 +0900

    [설정] 앱 v3.0.15 버전으로 수정

--- App/Main/Home/Setting/Setting.js
+++ App/Main/Home/Setting/Setting.js
@@ -114,7 +114,7 @@ export function Setting({ navigation }) {
         </TouchableOpacity>
       </View>
       <View style={styles.versionViewStyle}>
-        <Text style={styles.versionStyle}>v.3.0.14</Text>
+        <Text style={styles.versionStyle}>v.3.0.15</Text>
       </View>
     </View>
   );
